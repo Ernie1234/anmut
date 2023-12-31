d4f254eefd7a006959d7cba0ee679f3339c99aa7
Ernie1234 <anumahjoshuaeneye@gmail.com>
2023-12-31 01:46:17 +0100

    about

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Card } from "./Card";
 export { default as Team } from "./Team";
 export { default as Banner } from "./Banner";
 export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
 export { default as Section } from "./Section";
 export { default as CompanyInfo } from "./CompanyInfo";
 export { default as Jumbotron } from "./Jumbotron";
