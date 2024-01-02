2531c284bca7456b006a4f75285853a832c2d269
unknown <anumahjoshuaeneye@gmail.com>
2024-01-02 15:45:12 +0100

    about page finished

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -21,9 +21,9 @@ function About() {
         </div>
       </header>
 
-      <section className="bg-slate-800 text-white pt-8">
-        <div className="px-4 md:px-8 lg:px-12 mx-auto max-w-screen-xl grid grid-cols-1 md:grid-cols-2 gap-10 p-10">
-          <div className="w-full h-full rounded-3xl saturate-50 hover:filter-none hover:rounded-md overflow-hidden skew-y-3 transition-all hover:skew-y-0  ">
+      <section className="bg-slate-800 text-white py-8 md:py-12">
+        <div className="px-4 md:px-8 lg:px-12 mx-auto max-w-screen-xl grid grid-cols-1 md:grid-cols-2 gap-10 p-2 md:p-10">
+          <div className="w-full h-full rounded-3xl saturate-50 hover:filter-none hover:rounded-md overflow-hidden skew-y-3 transition-all hover:skew-y-0">
             <img
               className=" max-w-full h-full object-cover"
               src="https://images.pexels.com/photos/1216589/pexels-photo-1216589.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -34,8 +34,8 @@ function About() {
             <h1 className="mb-2 text-xl md:text-2xl lg:text-3xl font-bold tracking-wide underline">
               who we are
             </h1>
-            <p className="mt-3 leading-normal">leading-relaxed</p>
-            <p>
+
+            <p className="mt-5 leading-normal">
               An estate and property development company that specialize in
               creating exceptional and sustainable real estate projects that
               meets the need of a modern society. We utilize sustainable
@@ -43,62 +43,68 @@ function About() {
               projects are energy-efficient, cost-effective, and completed on
               time and on budget.
             </p>
-            <p className="mt-3">
+            <p className="mt-3 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Consequuntur laboriosam nulla quibusdam velit aliquam inventore.
             </p>
           </div>
         </div>
       </section>
-      <section className="vision__story">
-        <div className="grid grid-cols-2 gap-5">
-          <div className="about__section--content">
-            <h1 className="mb-2">Our Vision</h1>
-            <p className="mb-1">
+      <section className="bg-slate-900 text-white py-8 md:py-12">
+        <div className="px-4 md:px-8 lg:px-12 mx-auto max-w-screen-xl grid grid-cols-1 md:grid-cols-2 gap-10 p-2 md:p-10">
+          <div className="flex flex-col justify-center items-start ">
+            <h1 className="mb-2 text-xl md:text-2xl lg:text-3xl font-bold tracking-wide underline">
+              Our Vision
+            </h1>
+            <p className="mt-5 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
               fugit aliquam repudiandae fugiat aperiam dolor modi rem asperiores
               dolorum incidunt sequi dolores quasi natus odio explicabo adipisci
               commodi nobis nulla doloremque illo. Eum optio molestiae ipsam nam
               natus nulla nobis!
             </p>
-            <p>
+            <p className="mt-3 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
               mollitia nesciunt temporibus ad officia eveniet consequuntur ab
               quod culpa dignissimos!
             </p>
           </div>
-          <div className="rounded-full overflow-hidden skew-y-6 transition-all hover:skew-y-0">
+          <div className="w-full h-full rounded-3xl saturate-50 hover:filter-none hover:rounded-md overflow-hidden skew-y-3 transition-all hover:skew-y-0">
             <img
               src="https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt="StoryImage"
+              className=" max-w-full h-full object-cover"
             />
           </div>
         </div>
       </section>
 
-      <section className="about__mission">
-        <div className="container about__story--container">
-          <div className="rounded-full overflow-hidden skew-y-6 transition-all hover:skew-y-0">
+      <section className="bg-slate-800 text-white py-8 md:py-12">
+        <div className="px-4 md:px-8 lg:px-12 mx-auto max-w-screen-xl grid grid-cols-1 md:grid-cols-2 gap-10 p-2 md:p-10">
+          <div className="w-full h-full rounded-3xl saturate-50 hover:filter-none hover:rounded-md overflow-hidden skew-y-3 transition-all hover:skew-y-0">
             <img
               src="https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt="missionImage"
+              className=" max-w-full h-full object-cover"
             />
           </div>
-          <div className="about__section--content">
-            <h1>Our Mission</h1>
-            <p>
+          <div className="flex flex-col justify-center items-start ">
+            <h1 className="mb-2 text-xl md:text-2xl lg:text-3xl font-bold tracking-wide underline">
+              Our Mission
+            </h1>
+            <p className="mt-5 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
               fugit aliquam repudiandae fugiat aperiam dolor modi rem asperiores
               dolorum incidunt sequi dolores quasi natus odio explicabo adipisci
               commodi nobis nulla doloremque illo. Eum optio molestiae ipsam nam
               natus nulla nobis!
             </p>
-            <p>
+            <p className="mt-3 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
               mollitia nesciunt temporibus ad officia eveniet consequuntur ab
               quod culpa dignissimos!
             </p>
-            <p>
+            <p className="mt-3 leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Consequuntur laboriosam nulla quibusdam velit aliquam inventore.
             </p>
