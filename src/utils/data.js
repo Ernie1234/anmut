2057b09aa47e8c0a3abf10b0dc00107a67b5af18
unknown <anumahjoshuaeneye@gmail.com>
2024-03-27 11:47:36 +0100

    team update

--- src/utils/data.js
+++ src/utils/data.js
@@ -6,7 +6,7 @@ export const teamMembers = [
     job: "Director",
     url: "https://www.linkedin.com/in/folajomi/?originalSubdomain=ng",
     avatar:
-      "https://media.licdn.com/dms/image/D4D03AQH-VxALH44CKg/profile-displayphoto-shrink_400_400/0/1673524136547?e=1710979200&v=beta&t=6ybdib8se6UZEOYOoL_4k953P3JGw_F7kVPj1YmD_B8",
+      "https://media.licdn.com/dms/image/D4D03AQH-VxALH44CKg/profile-displayphoto-shrink_800_800/0/1673524136547?e=1717027200&v=beta&t=0wAtKur_A-ij4-cwJKXtdIJa8XnxJ8ZQdcm2Eq5R1o0",
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ export const teamMembers = [
     job: "Project Manager",
     url: "https://www.linkedin.com/in/funsho-olowu/",
     avatar:
-      "https://media.licdn.com/dms/image/C4D03AQFzYP4hA8XrFQ/profile-displayphoto-shrink_400_400/0/1565643752475?e=1710979200&v=beta&t=zAnEkQVc8Yaczp98baI0IY-dWBFrkY3oRp2qMZcATOg",
+      "https://media.licdn.com/dms/image/C4D03AQFzYP4hA8XrFQ/profile-displayphoto-shrink_800_800/0/1565643752532?e=1717027200&v=beta&t=2zdUr7iSTwjZ-mh1WV_PMIkwVsBCJY7hZ1aX1eC6sSI",
   },
   {
     id: 3,
@@ -34,16 +34,49 @@ export const teamMembers = [
     job: "Creative Writer & Content Creator",
     url: "https://www.linkedin.com/in/pelumi-olaosebikan/?originalSubdomain=ng",
     avatar:
-      "https://media.licdn.com/dms/image/C4E03AQEavFtHSRLh0w/profile-displayphoto-shrink_800_800/0/1646264564888?e=1710979200&v=beta&t=akkSGseAdVdXq1Ei9on0wqgCT-Fn9lk1BZi1B6f4m50",
+      "https://media.licdn.com/dms/image/C4E03AQEavFtHSRLh0w/profile-displayphoto-shrink_800_800/0/1646264564888?e=1717027200&v=beta&t=WWYw8wsu_9CwyInBauzB6e8UOcO7wHGUWGc2VqlzLo8",
   },
   {
     id: 5,
-    name: "Victor Akomolehin",
-    quote:
-      "Mass Communicator || Project Officer || Creative Writer & Content Creator || ||Human Resource Personnel|| || Presenter || Blogger ||Editor",
-    job: "Creative Writer & Content Creator",
-    url: "https://www.linkedin.com/in/pelumi-olaosebikan/?originalSubdomain=ng",
+    name: "QS VICTOR (B) AKOMOLEHIN",
+    quote: "Team lead at Vicknik Limited",
+    url: "https://www.linkedin.com/in/qs-victor-akomolehin-pmniqs-cpm-acipm-pmp-prince2-mba-33049712b/?trk=opento_nprofile_details",
     avatar:
-      "https://media.licdn.com/dms/image/C4E03AQEavFtHSRLh0w/profile-displayphoto-shrink_800_800/0/1646264564888?e=1710979200&v=beta&t=akkSGseAdVdXq1Ei9on0wqgCT-Fn9lk1BZi1B6f4m50",
+      "https://media.licdn.com/dms/image/D4D35AQEKvrOoPtW_xw/profile-framedphoto-shrink_400_400/0/1708255865892?e=1712142000&v=beta&t=V6ogM-nQOmcwdBfQf4Lc_3tvO4k_gKG1FFSr9OLE734",
+    job: "(PMNIQS) ,CPM,ACIPM, PMP, PRINCE2.MBA",
+  },
+  {
+    id: 6,
+    name: "Blessing Ewemade",
+    quote: "Agricultural Economist / CEO Boie Farms /Admin Manager",
+    url: "https://www.linkedin.com/in/blessing-ewemade-7aa417148/",
+    avatar:
+      "https://media.licdn.com/dms/image/C4D03AQEXdnoiYG7ubg/profile-displayphoto-shrink_400_400/0/1573234883610?e=1717027200&v=beta&t=vhXcKa6-Ax72lV472TR7QORrLzWAgAaAeVivlPDTXl8",
+  },
+  {
+    id: 7,
+    name: "Christopher Akinmukomi",
+    quote: "Site Supervisor",
+    url: "https://www.linkedin.com/in/qs-victor-akomolehin-pmniqs-cpm-acipm-pmp-prince2-mba-33049712b/?trk=opento_nprofile_details",
+    avatar: "/assets/team1.jpg",
+  },
+  {
+    id: 8,
+    name: "Animasaun Jephthah",
+    quote: "Sales Cashier at Eben Taiwo Nigeria Enterprise",
+    url: "https://www.linkedin.com/in/animasaun-jephthah-90759a205/",
+    avatar:
+      "https://media.licdn.com/dms/image/C4E03AQElZgUCFiWvNw/profile-displayphoto-shrink_800_800/0/1612168110829?e=1717027200&v=beta&t=MNd6wOxvWkveuWLZ8VPTN7YDPChSXQRxLmb6p1-3028",
+    job: "Sales Cashier at Eben Taiwo Nigeria Enterprise",
+  },
+  {
+    id: 9,
+    name: "Oluwadamilola Akintola",
+    quote:
+      "HR Officer/Executive Assistant| Transcriber| Art Enthusiast| Writer",
+    url: "https://www.linkedin.com/in/animasaun-jephthah-90759a205/",
+    avatar:
+      "https://media.licdn.com/dms/image/D4D03AQH3YyeWB1Qq-Q/profile-displayphoto-shrink_800_800/0/1702295749497?e=1717027200&v=beta&t=8ppv-ZdQavFnwN8J-tn9VrmaAfh-ypuf1E3GxPQOkys",
+    job: "HR Officer/Executive Assistant| Transcriber| Art Enthusiast| Writer.",
   },
 ];
