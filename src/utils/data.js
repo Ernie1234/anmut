34219caed8ed947f8e4c4ebab58861f78cde7f1f
unknown <anumahjoshuaeneye@gmail.com>
2024-04-03 13:53:16 +0100

    job update

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,18 +1,7 @@
 export const teamMembers = [
   {
-    id: 1,
-    name: "Folajomi Adegbulugbe",
-    quote: "Business Management | Investments | Strategy | Real Estate",
-    job: "Director",
-    url: "https://www.linkedin.com/in/folajomi/?originalSubdomain=ng",
-    avatar:
-      "https://media.licdn.com/dms/image/D4D03AQH-VxALH44CKg/profile-displayphoto-shrink_800_800/0/1673524136547?e=1717027200&v=beta&t=0wAtKur_A-ij4-cwJKXtdIJa8XnxJ8ZQdcm2Eq5R1o0",
-  },
-  {
     id: 2,
     name: "Funsho Olowu",
-    quote:
-      "Project Management | Real Estate & Property Development | Infrastructure Development & Finance",
     job: "Project Manager",
     url: "https://www.linkedin.com/in/funsho-olowu/",
     avatar:
@@ -21,7 +10,7 @@ export const teamMembers = [
   {
     id: 3,
     name: "Paul Ayanda",
-    quote: "Architect at Polays Int'l Concepts Ltd",
+
     job: "Lead Architect",
     avatar: "/assets/lead architech.jpg",
     url: "https://www.linkedin.com/in/a-o-paul-65944277/?originalSubdomain=ng",
@@ -29,8 +18,6 @@ export const teamMembers = [
   {
     id: 4,
     name: "Pelumi Olaosebikan",
-    quote:
-      "Mass Communicator || Project Officer || Creative Writer & Content Creator || ||Human Resource Personnel|| || Presenter || Blogger ||Editor",
     job: "Creative Writer & Content Creator",
     url: "https://www.linkedin.com/in/pelumi-olaosebikan/?originalSubdomain=ng",
     avatar:
@@ -39,16 +26,15 @@ export const teamMembers = [
   {
     id: 5,
     name: "QS VICTOR (B) AKOMOLEHIN",
-    quote: "Team lead at Vicknik Limited",
     url: "https://www.linkedin.com/in/qs-victor-akomolehin-pmniqs-cpm-acipm-pmp-prince2-mba-33049712b/?trk=opento_nprofile_details",
     avatar:
       "https://media.licdn.com/dms/image/D4D35AQEKvrOoPtW_xw/profile-framedphoto-shrink_400_400/0/1708255865892?e=1712142000&v=beta&t=V6ogM-nQOmcwdBfQf4Lc_3tvO4k_gKG1FFSr9OLE734",
-    job: "(PMNIQS) ,CPM,ACIPM, PMP, PRINCE2.MBA",
+    job: "Team Lead",
   },
   {
     id: 6,
     name: "Blessing Ewemade",
-    quote: "Agricultural Economist / CEO Boie Farms /Admin Manager",
+    job: "Admin Manager",
     url: "https://www.linkedin.com/in/blessing-ewemade-7aa417148/",
     avatar:
       "https://media.licdn.com/dms/image/C4D03AQEXdnoiYG7ubg/profile-displayphoto-shrink_400_400/0/1573234883610?e=1717027200&v=beta&t=vhXcKa6-Ax72lV472TR7QORrLzWAgAaAeVivlPDTXl8",
@@ -56,27 +42,25 @@ export const teamMembers = [
   {
     id: 7,
     name: "Christopher Akinmukomi",
-    quote: "Site Supervisor",
+    job: "Site Supervisor",
     url: "https://www.linkedin.com/in/qs-victor-akomolehin-pmniqs-cpm-acipm-pmp-prince2-mba-33049712b/?trk=opento_nprofile_details",
     avatar: "/assets/team1.jpg",
   },
   {
     id: 8,
     name: "Animasaun Jephthah",
-    quote: "Sales Cashier at Eben Taiwo Nigeria Enterprise",
     url: "https://www.linkedin.com/in/animasaun-jephthah-90759a205/",
     avatar:
       "https://media.licdn.com/dms/image/C4E03AQElZgUCFiWvNw/profile-displayphoto-shrink_800_800/0/1612168110829?e=1717027200&v=beta&t=MNd6wOxvWkveuWLZ8VPTN7YDPChSXQRxLmb6p1-3028",
-    job: "Sales Cashier at Eben Taiwo Nigeria Enterprise",
+    job: "Sales Cashier",
   },
   {
     id: 9,
     name: "Oluwadamilola Akintola",
-    quote:
-      "HR Officer/Executive Assistant| Transcriber| Art Enthusiast| Writer",
+
     url: "https://www.linkedin.com/in/animasaun-jephthah-90759a205/",
     avatar:
       "https://media.licdn.com/dms/image/D4D03AQH3YyeWB1Qq-Q/profile-displayphoto-shrink_800_800/0/1702295749497?e=1717027200&v=beta&t=8ppv-ZdQavFnwN8J-tn9VrmaAfh-ypuf1E3GxPQOkys",
-    job: "HR Officer/Executive Assistant| Transcriber| Art Enthusiast| Writer.",
+    job: "HR Officer/Executive Assistant",
   },
 ];
